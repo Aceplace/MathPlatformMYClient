aeb8d730ce007e4886c21ed76f4537ff54597be6
MIKE-DESKTOP\Mike <aceplace1116@gmail.com>
2025-08-06 11:13:00 -0400

    Initial commit

---
+++ MathPlatformMYClient/Program.cs
@@ -0,0 +1,34 @@
+﻿using ImGuiNET;
+using MCYRay_cs.Auto.Menu;
+using Raylib_cs;
+
+namespace MCYRay_cs;
+
+public static class MainTemplate
+{
+    public static void Main()
+    {
+        MCYRay.MCYRayInit(640, 480, "Math Platform - My Client");
+        
+        bool shouldQuit = false;
+        while (!Raylib.WindowShouldClose() && !shouldQuit)
+        {
+            MCYRay.BeginFrame();
+
+            if (ImGui.BeginMainMenuBar())
+            {
+                if (ImGui.BeginMainMenuBar())
+                {
+                    AutoMenus.DoAutoMenus();
+                    ImGui.EndMainMenuBar();
+                }
+            }
+
+            ImGui.DockSpaceOverViewport(ImGui.GetMainViewport());
+        
+            MCYRay.EndFrame();
+        }
+        
+        MCYRay.MCYRayShutdown();
+    }
+}
